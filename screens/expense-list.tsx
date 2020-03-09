e43e3f00a7d0a4d3350555a45fb87d0c90779f12
Elly Kitoto <junkmailstoelly@gmail.com>
2020-03-10 01:07:44 +0300

    Start implementation on expense filter
    Added TextInput field for filtering expenses by
    merchant and user first and last name

--- screens/expense-list.tsx
+++ screens/expense-list.tsx
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import {
     ActivityIndicator,
     FlatList,
+    NativeEventEmitter,
+    NativeModules,
     StyleSheet,
     Text,
+    TextInput,
     TouchableOpacity,
     View,
-    NativeEventEmitter,
 } from 'react-native';
 import ItemSeparator from "../components/item-separator";
 import {Expense} from "../common/common-types";
 import {connect} from "react-redux";
 import ExpenseService from "../services/expense-service";
 import {AppState} from "../store";
-import {NativeModules} from 'react-native';
 import moment from "moment";
 
 interface Props {
@@ -22,18 +23,25 @@ interface Props {
     isLoading: boolean
 }
 
-class ExpenseList extends Component<Props> {
+interface State {
+    filterText: string
+    filteredExpenses: Expense []
+}
+
+class ExpenseList extends Component<Props, State> {
 
     private expenseService = new ExpenseService();
 
     constructor(props: Props) {
         super(props);
+        this.state = {filteredExpenses: this.props.expenses, filterText: ''}
     }
 
     componentDidMount(): void {
         this.expenseService.fetchExpenses()
             .then(expenses => {
                 this.expenseService.dispatchSaveExpenses(expenses);
+                this.setState({filteredExpenses: expenses})
             }).catch(error => console.log(error));
 
         const eventEmitter = new NativeEventEmitter(NativeModules.ExpenseDetailsModule);
@@ -103,6 +111,14 @@ class ExpenseList extends Component<Props> {
         NativeModules.ExpenseDetailsModule.displayExpenseDetails(item);
     }
 
+    private filterExpenses(text: string) {
+        this.setState({filteredExpenses: this.props.expenses, filterText: text.toLowerCase().trim()});
+        const currentExpenses = this.state.filteredExpenses.filter(expense => {
+            return expense.merchant.toLowerCase().trim().match(this.state.filterText);
+        });
+        this.setState({filteredExpenses: currentExpenses})
+    }
+
     render() {
         if (this.props.isLoading) {
             return (
@@ -113,9 +129,16 @@ class ExpenseList extends Component<Props> {
         }
         return (
             <View style={styles.container}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        onChangeText={this.filterExpenses.bind(this)}
+                        placeholder={"Search by merchant/user"}
+                        style={styles.search}/>
+                </View>
+
                 <FlatList
                     ItemSeparatorComponent={ItemSeparator}
-                    data={this.props.expenses}
+                    data={this.state.filteredExpenses}
                     renderItem={data => this.bindItem(data.item)}
                     keyExtractor={item => item.id.toString()}
                 />
@@ -174,8 +197,15 @@ const styles = StyleSheet.create({
     date: {
         fontSize: 14,
         color: '#696969',
-    }
-
+    },
+    search: {
+        padding: 14,
+        marginStart: 8,
+        fontSize: 16
+    },
+    searchWrapper: {
+        backgroundColor: '#F3F4F5',
+    },
 });
 
 
